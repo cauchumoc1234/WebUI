7e2fd720c8dfdca936c5ca5b4682ffa96353b800
cauchumoc1234 <ducanhfo3888@gmail.com>
2020-10-14 14:52:36 +0700

    Module db and file env

---
+++ db.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('DB connected');
+});
+module.exports = db;
